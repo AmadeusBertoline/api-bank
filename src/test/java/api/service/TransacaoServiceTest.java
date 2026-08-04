27583344eae95e918f0600382fe2de76588e09eb
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2026-08-04 13:57:26 -0300

    chore: removendo imports indesejados

--- src/test/java/api/service/TransacaoServiceTest.java
+++ src/test/java/api/service/TransacaoServiceTest.java
@@ -82,12 +82,9 @@ class TransacaoServiceTest {
     private Conta contaDestino;
     private ChavePix chavePixDestino;
     private TransacaoResponseDTO transacaoResponseDTO;
-    private String idempotencyKey;
-
+   
     @BeforeEach
     void setup() {
-
-        idempotencyKey = "123e4567-e89b-12d3-a456-426614174000";
 
         // CONTA ORIGEM
 
