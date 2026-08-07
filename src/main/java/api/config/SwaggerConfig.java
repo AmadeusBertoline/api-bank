d3c1c1f8f80fabd00b52a95b8d0ab40222be5ad0
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2026-08-07 10:34:19 -0300

    deploy: ajustando ordem de tags do swagger

--- src/main/java/api/config/SwaggerConfig.java
+++ src/main/java/api/config/SwaggerConfig.java
@@ -1,17 +1,17 @@
 package api.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
-
-import java.util.List;
-
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -26,6 +26,16 @@ public class SwaggerConfig {
                         .contact(new Contact()
                                 .name("Amadeus Bertoline")
                                 .email("amadeusbertoline123@gmail.com")))
+
+                .tags(List.of(
+                        new Tag().name("Autenticação").description("Endpoints de login e geração de token"),
+                        new Tag().name("Contas").description("Operações e consultas de contas bancárias"),
+                        new Tag().name("Usuários").description("Gerenciamento e visualização de dados do usuário"),
+                        new Tag().name("Chaves Pix").description("Gestão de chaves Pix"),
+                        new Tag().name("Transações").description("Realizar transação e extrato"),
+                        new Tag().name("Admin").description("Cadastro de administradores, gerenciamento de contas e visualização de dados de usuários")
+                ))
+
                 .addSecurityItem(new SecurityRequirement().addList("Bearer"))
                 .servers(List.of(new Server().url("/")))
                 .components(new Components()
